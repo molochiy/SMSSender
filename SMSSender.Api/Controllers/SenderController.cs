5dd1f58327897169a9ceed488af57c12fd5304a4
molochiy <molochiy@gmail.com>
2018-08-22 01:16:43 +0300

    Add project files.

---
+++ SMSSender.Api/Controllers/SenderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMSSender.Api.Controllers
+{
+    [Route("")]
+    public class SenderController : Controller
+    {
+        [Route("SendSMS")]
+        // TODO: for testing purposes, change action method to HttpPost and launchUrl to some default
+        [HttpGet]
+        public async Task<IActionResult> SendSms([FromQuery]string msg, [FromQuery]DateTime? startDate, [FromQuery]DateTime? endDate)
+        {
+            string result = await Task.FromResult($"Message [{msg}] sent for users between {startDate} and {endDate}.")
+                .ConfigureAwait(true);
+
+            return Ok(result);
+        }
+    }
+}
